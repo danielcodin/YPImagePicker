e5738ac44b2cf5d237cac724353d7b7b8ced70bb
Daniel Tseng <daniel@danielcoding.com>
2020-02-14 16:40:42 +0800

    Makes photo filter upper right button be done

--- Source/Filters/YPPhotoFiltersVC.swift
+++ Source/Filters/YPPhotoFiltersVC.swift
@@ -102,7 +102,8 @@ open class YPPhotoFiltersVC: UIViewController, IsMediaFilterVC, UIGestureRecogni
     // MARK: Setup - ⚙️
     
     fileprivate func setupRightBarButton() {
-        let rightBarButtonTitle = isFromSelectionVC ? YPConfig.wordings.done : YPConfig.wordings.next
+        //let rightBarButtonTitle = isFromSelectionVC ? YPConfig.wordings.done : YPConfig.wordings.next
+        let rightBarButtonTitle = YPConfig.wordings.done
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: rightBarButtonTitle,
                                                             style: .done,
                                                             target: self,
